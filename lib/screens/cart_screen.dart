c58f30c1cda454828222e9aa78a4cbb57b95e648
EdYuTo <edson.toma@usp.br>
2020-02-13 10:37:58 -0200

    add remove item from cart method

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -51,6 +51,7 @@ class CartScreen extends StatelessWidget {
               itemCount: cart.itemCount,
               itemBuilder: (ctx, idx) => CartItem(
                 id: cart.items.values.toList()[idx].id,
+                productId: cart.items.keys.toList()[idx],
                 title: cart.items.values.toList()[idx].title,
                 price: cart.items.values.toList()[idx].price,
                 quantity: cart.items.values.toList()[idx].quantity,
