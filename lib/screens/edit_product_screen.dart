1f6146dde7ed573835406b7fc637d552a2d80198
EdYuTo <edson.toma@usp.br>
2020-02-14 08:52:19 -0200

    finish edit product screen display setup

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product-screen';
+
   @override
   _EditProductScreenState createState() {
     return _EditProductScreenState();
@@ -9,11 +10,33 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
+    _imageUrlFocusNode.addListener(_updateImageUrl);
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl); // avoid memory leaks
+    _priceFocusNode.dispose(); // avoid memory leaks
+    _descriptionFocusNode.dispose(); // avoid memory leaks
+    _imageUrlFocusNode.dispose(); // avoid memory leaks
+    _imageUrlController.dispose(); // avoid memory leaks
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +51,55 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                focusNode: _priceFocusNode,
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                focusNode: _descriptionFocusNode,
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType:
+                    TextInputType.multiline, // automatically gives enter symbol
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter an url')
+                        : Image.network(_imageUrlController.text, fit: BoxFit.cover,),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      focusNode: _imageUrlFocusNode,
+                      controller: _imageUrlController,
+                      decoration: InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
