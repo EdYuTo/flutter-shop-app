baf7aa48f0b13cf9d1fe52cb894ef0a38f038137
EdYuTo <edson.toma@usp.br>
2020-02-17 14:38:29 -0300

    using FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -12,35 +12,30 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchOrders();
-      _isLoading = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+        builder: (ctx, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting)
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, idx) => item.OrderItem(orderData.orders[idx]),
-            ),
+            );
+          else if (dataSnapShot.error == null)
+            return Consumer<Orders>(
+              builder: (ctx, orderData, child) => ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctx, idx) =>
+                    item.OrderItem(orderData.orders[idx]),
+              ),
+            );
+          return null;
+        },
+      ),
       drawer: AppDrawer(),
     );
   }
